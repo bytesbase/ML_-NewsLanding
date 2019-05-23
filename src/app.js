9604a8045104c9ed22289bea1c53950553dbf7be
vincentdecaster <decastervincent@gmail.com>
2019-05-23 17:55:48 -0500

    UX++ for input, edited robot to hide on sandbox

--- src/app.js
+++ src/app.js
@@ -2,3 +2,14 @@
 import './style.scss';
 import './assets/newsletter.gif';
 import './assets/favicons/favicon-16x16.png';
+
+
+document.getElementById("email").addEventListener("keyup", function() {
+    var emailInput = document.getElementById('email').value;
+    if (emailInput != "") {
+        document.getElementById('subscribe').removeAttribute("disabled");
+    } else {
+        document.getElementById('subscribe').setAttribute("disabled", null);
+    }
+});
+
